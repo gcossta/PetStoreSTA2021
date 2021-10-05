03423256e110ee84fb30f2cce05a5c73c8ce040a
Gabriel Costa <gabriel.costa@amaggi.com.br>
2021-10-04 21:39:57 -0400

    Finalizado script excluirPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -76,6 +76,25 @@ public class Pet {
                 .body("status", is("sold"))
         ;
     }
+
+    @Test
+    public void excluirPet(){
+        String petId = "2612198712";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+        ;
+    }
+
 }
 
 
